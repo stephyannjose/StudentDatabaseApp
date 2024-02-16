9df6beb77b59acea3f6094b97aff21cb76893636
stephyannjose <stephyannjose@gmail.com>
2024-02-15 23:01:54 -0400

    Code Updated

--- StudentDatabaseApp/Program.cs
+++ StudentDatabaseApp/Program.cs
@@ -1,58 +1,69 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Runtime.Remoting.Contexts;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.EntityFrameworkCore;
+using System;
 
 namespace StudentDatabaseApp
 {
+    // Define your entity class
     public class Student
     {
         public int StudentId { get; set; }
         public string Name { get; set; }
         public DateTime DateOfBirth { get; set; }
     }
-}
 
-using Microsoft.EntityFrameworkCore;
-
-namespace StudentDatabaseApp
-{
+    // Create your DbContext class
     public class StudentDbContext : DbContext
     {
         public DbSet<Student> Students { get; set; }
 
+        // Configure the connection string
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer(@"Server=(localdb)\mssqllocaldb;Database=StudentDatabase;Trusted_Connection=True;");
         }
     }
-}
 
-using System;
-
-namespace StudentDatabaseApp
-{
     class Program
     {
         static void Main(string[] args)
         {
-            using (var context = new StudentDbContext())
+            try
             {
-                // Create a new student
-                var student = new Student
+                using (var context = new StudentDbContext())
                 {
-                    Name = "Stephy Jose",
-                    DateOfBirth = new DateTime(1988, 4, 12)
-                };
+                    // Create a new student
+                    var student = new Student
+                    {
+                        Name = "Stephy Ann Jose",
+                        DateOfBirth = new DateTime(1988, 4, 12)
+                    };
 
-                // Add the student to the database
-                context.Students.Add(student);
-                context.SaveChanges();
+                    // Add the student to the database
+                    context.Students.Add(student);
 
-                Console.WriteLine("Student added to the database.");
+                    // Save changes to the database
+                    context.SaveChanges();
+
+                    Console.WriteLine("Student added to the database.");
+                }
             }
+            catch (DbUpdateException ex)
+            {
+                // Print the outer exception message
+                Console.WriteLine("An error occurred while updating the entries: " + ex.Message);
+
+                // If there's an inner exception, print its message
+                if (ex.InnerException != null)
+                {
+                    Console.WriteLine("Inner Exception: " + ex.InnerException.Message);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+
+            Console.ReadKey();
         }
     }
 }
